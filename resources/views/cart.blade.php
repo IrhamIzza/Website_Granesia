3be894790b9716a1ca90fd4fe72f7b7704e7eb4f
IrhamIzza <Irhamizza21@gmail.com>
2022-06-04 14:21:45 +0700

    Tampilan Keranjang

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,25 +1,34 @@
-<div class="container">
+@extends('layouts.core2')
+@section('title', 'Cart')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<div class="row">
-    @foreach($cart as $key => $value)
-    <div class="col-lg-12 ">
-        <div class="bg-halbelanja2">
-            <center>
-                <div class="blog-block ">
-                    <img src="images/banner/belanja/{{$value ->picture}}" alt="" class="gambar img-fluid">
-                    <div class="blog-text">
-                        <h4>{{$value -> item_name}}</h4>
-                        <h5 class="text-white">{{$value ->price}}</h5><br>
-                        <p>total barang:{{$value -> quantity}}</p>
-                        <a href="#" class="btn4 btn-white btn-circled">beli sekarang</a>
-                    </div>
+<head>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/belanja.css') }}">
+</head>
+@section('belanja', 'active')
+</body>
+
+<h2 class="judul1">Garnesia | Keranjang</h2>
+<div class="container-fluid">
+    <div class="row gy-5">
+        @foreach($cart as $key => $value)
+        <div class="col-md-12 ">
+            <div class="bg-halbelanja2">
+                <img src="images/banner/belanja/{{$value ->picture}}" alt="" class="gambar">
+                <div class="blog-text">
+                    <h4 class="item">{{$value -> item_name}}</h4>
+                    <h5 class="harga">IDR {{$value ->price}}</h5><br>
+                    <p class="total">x{{$value -> quantity}}</p>
+                    <button href="/order" class="btn_beli">Beli Sekarang</button>
                 </div>
-            </center>
-        </div>
+            </div>
 
+
+        </div>
+        @endforeach
 
     </div>
-    @endforeach
+</div>
 
-</div>
-</div>
+<body>
--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -1,36 +1,62 @@
-<div class="container">
+@extends('layouts.core2')
+@section('title', 'Cart')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <div class="row">
-        @foreach($cart as $key => $value)
+<head>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/belanja.css') }}">
+</head>
+@section('belanja', 'active')
+<h2 class="judul1">Garnesia | Belanja | Checkout</h2>
+<!-- <p class="fw-bold fs-5" style="margin-left: 5%;">Alamat Pengirim</p> -->
+<div class="line"></div>
+<div class="row gy-5">
+    @foreach($cart as $key => $value)
+    <div class="col-md-12 ">
+        <div class="bg-halbelanja2">
+            <img src="images/banner/belanja/{{$value ->picture}}" alt="" class="gambar">
+            <div class="blog-text">
+                <h4 class="item">{{$value -> item_name}}</h4>
+                <h5 class="harga">IDR {{$value ->price}}</h5><br>
+                <p class="total">X{{$value -> quantity}}</p>
+                <button href="/order" class="btn_beli">Beli Sekarang</button>
+            </div>
+        </div>
         
-        <div class="col-lg-12 ">
+    </div>
+    @endforeach
+<!-- <div class="contain"> -->
+    <form method="post" action="{{url('payment/'. $user ->id)}}">
+        @csrf
+        <p class="fw-bold">{{$user -> name}} </p>
+        <p class="fw-bold">Alamat: <input type="text" name="address" value="{{$user -> address}}"></p>
+        <p class="fw-bold">Nomor hp: <input type="text" name="number_telp" value="{{$user -> number_telp}}"> </p>
+        <p class="fw-bold">Tanggal: {{$time}}</p>
+        <!-- <p class="fw-bold">Total: {{$total}}</p>
+            <p class="fw-bold">Note:<input type="text" name="note"> </p> -->
+    </form>
+    <!-- <div class="line" style="margin-left: 0%;"></div>
+    <div class="container-fluid">
+        @foreach($cart as $key => $value)
+        <div class="col-md-12 ">
             <div class="bg-halbelanja2">
-                <center>
-                    <div class="blog-block ">
-                        <img src="images/banner/belanja/{{$value ->picture}}" alt="" class="gambar img-fluid">
-                        <div class="blog-text">
-                            <h4>{{$value -> item_name}}</h4>
-                            <h5 class="text-white">{{$value ->price}}</h5><br>
-                            <p>total barang:{{$value -> quantity}}</p>
-                            <a href="#" class="btn4 btn-white btn-circled">beli sekarang</a>
-                        </div>
-                    </div>
-                </center>
+                <img src="images/banner/belanja/{{$value ->picture}}" alt="" class="gambar">
+                <div class="blog-text">
+                    <h4 class="item">{{$value -> item_name}}</h4>
+                    <h5 class="harga">IDR {{$value ->price}}</h5><br>
+                    <p class="quantity">X{{$value -> quantity}}</p>
+                </div>
             </div>
 
         </div>
         @endforeach
-        <form method="post" action="{{url('payment/'. $user ->id)}}">
-            @csrf
-        <p>nama: {{$user -> name}} </p>
-        <p>alamat:<input type="text" name="address" value="{{$user -> address}}"></p>
-        <p>nomor hp::<input type="text" name="number_telp" value="{{$user -> number_telp}}"> </p>
-        <p>tanggal: {{$time}}</p>
-        <p>total: {{$total}}</p>
-        <p>note::<input type="text" name="note"> </p>
 
-            <button type="submit" class="btn3 btn-white btn-circled ">beli</button>
-        </form>
+        <h5 class="fw-bold" style="margin-top: 5%;">Sub Total : {{$total}}</h5>
+        <div class="line" style="margin-left: 0%;"></div>
+        <div class="line" style="margin-left: 0%;"></div>
+        <button type="submit" class="fw-bold fs-5 btn_beli">Beli</button>
+    </div> -->
+<!-- </div> -->
 
-    </div>
+</div>
 </div>
